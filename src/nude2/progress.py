1b8a934a9b797ff2ea337ba5f9954ab1aa69d194
Matt 3 <not.mattowen@gmail.com>
2023-11-03 18:55:48 -0400

    :shaved_ice: tweak progress bar stuff

--- src/nude2/progress.py
+++ src/nude2/progress.py
@@ -2,30 +2,24 @@ import random
 import time
 import sys
 
+import tqdm
 
-class Progress(object):
-    """..."""
-
-    def __init__(self, iterable, prefix="", size=100, clear=True):
-        pass
-
-    def __iter__(self):
-        for i in self.iterable:
-            yield i
-            self.show()
-
-    def show(self):
-        pass
-
+from datetime import datetime
 
 class ProgressBar(object):
     """..."""
 
-    def __init__(self, limit, prefix="", size=100, clear=True):
+    def __init__(self, x, prefix="", size=100, clear=True):
+
+        if isinstance(x, int):
+            self.iter = range(x)
+        else:
+            self.iter = iter(x)
+
+        self.start_time = datetime.now()
         self.clear = clear
         self.prefix = prefix
         self.size = size
-        self.limit = limit
         self.current = 0
         self.line = self.prefix + "[" + " "*self.size + "]"
         self.show()
@@ -35,11 +29,25 @@ class ProgressBar(object):
         return f"{random.choice(eye)}_{random.choice(eye)}"
 
     def show(self):
-        pct = self.current / self.limit
+
+        # Percentage complete
+        pct = self.current / len(self.iter)
         pct = round(pct * self.size)
+
+        # Time elapsed
+        dur = datetime.now() - self.start_time
+
+        t = int(dur.total_seconds())
+        r = dur.total_seconds() - t
+
+        h = t // 3600
+        m = (t - h*3600) // 60
+        s = t - h*3600 - m*60
+        f = t - h*3600 - m*60 - s
+
         sys.stdout.write("\b"*len(self.line))
         self.line = self.prefix + "[" + "*"*pct + " "*(self.size-pct) + "] " + self.x_x()
-        self.line += f" {100*self.current/self.limit:0.2f}%\r"
+        self.line += f" {100*self.current/len(self.iter):0.2f}% [{h}:{m:02}:{s:02}] @ {datetime.now().strftime('%H:%M:%S')}\r"
         sys.stdout.write(self.line)
         sys.stdout.flush()
 
@@ -59,8 +67,15 @@ class ProgressBar(object):
         if self.clear:
             sys.stdout.write("\b"*len(self.line))
 
+class I(object):
+    def __iter__(self):
+        return self
+    def __next__(self):
+        return 1
+
 def main():
-    with ProgressBar(300) as progress:
-        for _ in range(300):
-            time.sleep(0.02)
-            progress.inc()
+    for _ in range(5):
+        with ProgressBar(66) as progress:
+            for _ in range(66):
+                time.sleep(0.02)
+                progress.inc()
