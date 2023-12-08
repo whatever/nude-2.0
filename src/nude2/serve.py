6c0adb119b3d48efaa0427ba9b9c1a5fd27660c1
Matt 3 <not.mattowen@gmail.com>
2023-12-08 15:00:14 -0600

    :soon: pass in parameter to determine whether we use eval or not

--- src/nude2/serve.py
+++ src/nude2/serve.py
@@ -12,10 +12,13 @@ GENERATOR = Generator198x198()
 
 class Handler(http.server.SimpleHTTPRequestHandler):
 
-    def write_image(self):
-        noise = torch.randn(1, 100, 1, 1).to("cpu")
-        noise = torch.ones(1, 100, 1, 1).to("cpu")
-        arr = GENERATOR(noise)
+    def write_image(self, vec, train=False):
+
+        GENERATOR.train(train)
+
+        with torch.no_grad():
+            arr = GENERATOR(vec)
+
         img = MetCenterCroppedDataset.pilify(arr[0])
 
         res = io.BytesIO()
@@ -28,7 +31,8 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
 
         if self.path == "/api/v1/image":
-            self.write_image()
+            vec = torch.randn(1, 100, 1, 1).to("cpu")
+            self.write_image(vec)
             return
 
         if self.path != "/api/v1/query":
@@ -44,13 +48,29 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         })
         self.wfile.write(bytes(res.encode("ascii")))
 
+    def do_POST(self):
+
+        if self.path != "/api/v1/image":
+            return
+
+        content_len = int(self.headers.get('Content-Length'))
+        post_body = self.rfile.read(content_len)
+        data = json.loads(post_body.decode("ascii"))
+
+        if "vec" not in data:
+            self.send_response(400)
+            self.end_headers()
+            return
+
+        vec = torch.tensor(data["vec"]).reshape(1, 100, 1, 1).to("cpu")
+        self.write_image(vec, train=data.get("train", True))
+
 
 def main(checkpoint, port):
 
     print(f"loading checkpoint from {checkpoint}")
     STATES = torch.load(checkpoint, map_location=torch.device('cpu'))
     GENERATOR.load_state_dict(STATES["g"])
-    GENERATOR.train(False)
 
     print(f"serving at port {port}")
     httpd = http.server.HTTPServer(("", port), Handler)
